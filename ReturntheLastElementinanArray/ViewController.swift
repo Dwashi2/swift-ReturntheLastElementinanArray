20651cd9de8fc7e96a5ee1697828d26217948e6d
Daniel Washington <danielvertigo@hotmail.com>
2021-06-13 16:56:31 -0300

    Files

--- ReturntheLastElementinanArray/ViewController.swift
+++ ReturntheLastElementinanArray/ViewController.swift
@@ -5,6 +5,23 @@
 //  Created by Daniel Washington Ignacio on 13/06/21.
 //
 
+/*
+ Create a function that accepts an array and returns the last item in the array.
+
+ Examples
+
+ getLastItem([1, 2, 3]) ➞ 3
+
+ getLastItem(["cat", "dog", "duck"]) ➞ "duck"
+
+ getLastItem([true, false, true]) ➞ true
+ Notes
+
+ Don't forget to return the result.
+ If you get stuck on a challenge, find help in the Resources tab.
+ If you're really stuck, unlock solutions in the Solutions tab.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +29,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.getLastItem([1, 2, 3]))
+        print(self.getLastItem(["cat", "dog", "duck"]))
+        print(self.getLastItem([true, false, true]))
+    }
+    
+    func getLastItem(_ arr: [Any]) -> Any {
+        return arr.last
     }
 
 
